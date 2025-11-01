6194c3ae1e52e0a10981480c1838c62f764a1778
grooveyman <dee.grooveyman@gmail.com>
2025-11-01 17:43:08 +0000

    update to payment

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,58 +1,57 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useApiQuery } from "../hooks/useApi";
-import { CheckCircle } from "lucide-react";
+import { CheckCircle, XCircle } from "lucide-react";
 
 const Success: React.FC = () => {
   const { ref } = useParams();
-  const { data, isPending } = useApiQuery(
+  const { data, isPending, isError } = useApiQuery<{ status: boolean }>(
     [`chk_transaction_${ref}`],
     `/pay/success/${ref}`,
-    { refetchOnWindowFocus: false, refetchOnReconnect: false }
+    {enabled: !!ref, refetchOnWindowFocus: false, refetchOnReconnect: false, retry: false }
   );
-  console.log(data);
-  return (
-    <>
-      <div className="row">
-        <div className="col-md-3"></div>
-        <div className="col-md-6">
-          <div className="d-flex align-items-center justify-content-center vh-100">
-            <div className="card">
-              {isPending ? (
-                <div className="row d-flex justify-content-center p-5">
-                  <span
-                    className="spinner-border spinner-border"
-                    role="status"
-                  ></span>
-                </div>
-              ) : (
-                <div className="row">
-                  <CheckCircle size={50} />
-                  <h2 className="text-center mt-4">Payment Successful</h2>
-                  <p className="text-center">Thank you for buying from us.</p>
+  const navigate = useNavigate();
+  
 
-                  <div className="d-flex justify-content-center">
-                     <hr
-                    className="text-center"
-                    style={{ width: "50%", textAlign: "center" }}
-                  />
-                  </div>
-                 
-                  <p className="text-center p-3">
-                    Your order with Tracking No: xxxxx has been processed
-                    succesfully. Kindly check your email inbox or{" "}
-                    <a href="#">login</a> with your email to track your order.
-                  </p>
-                  <div className="d-flex justify-content-center">
-                    <button className="btn btn-primary">Track Order</button>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3"></div>
+  if (isPending) {
+    return (
+      <div className="d-flex align-items-center justify-content-center vh-100">
+        <span className="spinner-border" role="status"></span>
       </div>
-    </>
+    );
+  }
+
+    if (isError || data?.status === false) {
+    return (
+      <div className="d-flex align-items-center justify-content-center vh-100">
+        <div className="card p-4 text-center">
+            <div className="d-flex justify-content-center">
+                <XCircle size={50} />
+            </div>
+          
+          <h2 className="mt-4">Payment Failed</h2>
+          <p className="text-center p-3">We are sorry, payment was unsuccessful. Try again.</p>
+          <div className="d-flex justify-content-center">
+            <button className="btn btn-primary" onClick={() => navigate("/categories/shop")}>Back</button>
+          </div>
+          
+        </div>
+      </div>
+    );
+  }
+  
+return (
+    <div className="d-flex align-items-center justify-content-center vh-100">
+      <div className="card p-4 text-center">
+        <CheckCircle size={50} />
+        <h2 className="mt-4">Payment Successful</h2>
+        <p className="text-center">Thank you for buying from us.</p>
+        <hr style={{ width: "50%", margin: "0 auto" }} />
+        <p className="text-center p-3">
+          Your order was processed successfully. Check your email to track your order.
+        </p>
+        <button className="btn btn-primary">Track Order</button>
+      </div>
+    </div>
   );
 };
 
